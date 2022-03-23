c432f3610c1ef53044fc2c406ad8274cf3f0447c
Francesco-code-max <98326876+Francesco-code-max@users.noreply.github.com>
2022-03-23 13:05:08 +0300

    Modified my functions

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
     County Library System,
-    by, Francesco,
+    by, Francesco Cesaroni,
     Feb 2022,
     MIT License,
     C99 Compiler,
@@ -15,6 +15,13 @@ struct Patron
     char email[50];
     char password[30];
     int is_staff;
+};
+struct Book
+{
+    char name[100];
+    char title[50];
+    char author[30];
+    int is_available;
 };
 
 int menu();
@@ -32,6 +39,20 @@ void add_patron()
     printf("enter 1 if staff 0 otherwise: ");
     scanf("%d", &patron.is_staff);
     printf("Patron %s successfully added\n", patron.name);
+}
+void add_book()
+{
+    struct Book book;
+    printf("Enter book: ");
+    getchar();
+    gets(book.name);
+    printf("Enter title: ");
+    gets(book.title);
+    printf("Enter name of the author: ");
+    gets(book.author);
+    printf("enter 1 if book 0 otherwise: ");
+    scanf("%d", &book.is_available);
+    printf("book %s successfully added\n", book.name);
 }
 
 int main()
@@ -52,8 +73,8 @@ int menu ()
     printf("Select an action below\n");
     printf("1. Add new Patron\n");
     printf("2. view all patrons\n");
-    printf("3. Add new resources\n");
-    printf("4. View all resources\n");
+    printf("3. Add new book\n");
+    printf("4. View all books\n");
     printf("Your actions: \n");
     scanf("%d", &action);
     if(action < 1 || action > 4)
@@ -73,7 +94,7 @@ void execute_action(int action)
         printf(" view all patrons\n");
         break;
     case 3:
-        printf(" Add new resources\n");
+        add_book();
         break;
     case 4:
         printf(" View all resources\n");
@@ -83,3 +104,5 @@ void execute_action(int action)
 
     }
 }
+
+
